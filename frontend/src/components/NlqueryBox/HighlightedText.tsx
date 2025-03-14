acd438a7c55faac606a3d13ef6f4a060a8ee9aae
TuGitee <tgb@std.uestc.edu.cn>
2025-03-14 22:17:29 +0800

    fix: highlight with language difference

--- frontend/src/components/NlqueryBox/HighlightedText.tsx
+++ frontend/src/components/NlqueryBox/HighlightedText.tsx
@@ -12,7 +12,7 @@ interface HighlightedTextProps {
 const HighlightedText = memo(({ text, colorMap, query, onToggleDisabled }: HighlightedTextProps) => {
     if (!text || !colorMap || !query) return <>{text}</>;
 
-    const isEnglish = text.match(/^[a-zA-Z0-9 ]+$/);
+    const isEnglish = /^[a-zA-Z0-9 ,.\-!?;:'"()\\/&@#$%+=<>{}[\]|~`*]+$/.test(text);
 
     const highlights = Object.entries(query.text_sources).reduce<Array<{
         index: number;
@@ -23,7 +23,6 @@ const HighlightedText = memo(({ text, colorMap, query, onToggleDisabled }: Highl
         disabled: boolean;
     }>>((acc, [text_source_id, textSource]) => {
         if (!colorMap[text_source_id]) return acc;
-
         const regex = isEnglish ? new RegExp(`\\b${textSource.text.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&')}\\b`, 'g') : new RegExp(`${textSource.text.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&')}`, 'g');
         let match;
         const matches: number[] = [];
